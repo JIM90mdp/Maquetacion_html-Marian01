174a4c13f37d37b94d0fd5bb9985d01c237463e4
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-06-22 22:33:32 -0300

    add/feat: next-build

---
+++ next_build/out/_next/static/chunks/app/page-c5ca71fa910024fe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{837:function(n,e,u){Promise.resolve().then(u.t.bind(u,6342,23))},6342:function(){}},function(n){n.O(0,[667,488,744],function(){return n(n.s=837)}),_N_E=n.O()}]);
